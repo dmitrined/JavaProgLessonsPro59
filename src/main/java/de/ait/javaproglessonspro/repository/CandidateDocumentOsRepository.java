515afaf2cacb2d6f42e8dae0b0e4b05f2dcfbf02
dmitrined <dm.nedioglo@gmail.com>
2026-02-13 10:58:29 +0100

    add Spring_Boot_guide.md

--- src/main/java/de/ait/javaproglessonspro/repository/CandidateDocumentOsRepository.java
+++ src/main/java/de/ait/javaproglessonspro/repository/CandidateDocumentOsRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CandidateDocumentOsRepository extends JpaRepository<CandidateDocumentOs, Long> {
+
     List<CandidateDocumentOs> findAllByCandidateEmail(String email);
+
     void deleteAllByCandidateEmail(String email);
 }
